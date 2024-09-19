9f8980b994693040fa2444aca8c77ee15c227cdc
Demonq2004 <grzegorzekdawid@gmail.com>
2024-09-19 15:11:02 +0200

    commit

---
+++ src/api/photo-requirements/controllers/photo-requirements.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * photo-requirements controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::photo-requirements.photo-requirements');
---
+++ src/api/photo-requirements/services/photo-requirements.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * photo-requirements service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::photo-requirements.photo-requirements');
